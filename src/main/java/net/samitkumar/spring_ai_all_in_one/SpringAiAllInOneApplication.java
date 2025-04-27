76abfe3cd467d0dc8688170ef9271e0ee8720e0d
samitkumarpatel <samitkumarpatel@gmail.com>
2025-04-27 23:08:56 +0200

    added few more scenario

--- src/main/java/net/samitkumar/spring_ai_all_in_one/SpringAiAllInOneApplication.java
+++ src/main/java/net/samitkumar/spring_ai_all_in_one/SpringAiAllInOneApplication.java
@@ -6,21 +6,21 @@ import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.AbstractChatMemoryAdvisor;
 import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
 import org.springframework.ai.chat.client.advisor.PromptChatMemoryAdvisor;
+import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
 import org.springframework.ai.chat.memory.InMemoryChatMemory;
 import org.springframework.ai.tool.annotation.Tool;
 import org.springframework.ai.tool.annotation.ToolParam;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
@@ -32,31 +32,48 @@ public class SpringAiAllInOneApplication {
 	}
 
 	@Bean
+	Map<String, PromptChatMemoryAdvisor> promptStore() {
+		return new ConcurrentHashMap<>();
+	}
+
+	@Bean
 	ChatClient chatClient(ChatClient.Builder builder, BookingTool bookingTool) {
 		return builder
 				.defaultSystem("""
-							You are a customer chat support agent of an airline named "Funnair". Respond in a friendly,
-							helpful, and joyful manner.
+						You are a customer chat support agent of an airline named "Funnair". Respond in a friendly,
+						helpful, and joyful manner.
 
-							Before providing information about a booking or cancelling a booking, you MUST always
-							get the following information from the user: booking number.
+						Before providing information about a booking or cancelling a booking, you MUST always
+						get the following information from the user: booking number, customer first name and last name.
 
-							Before changing a booking you MUST ensure it is permitted by the terms.
+						Before changing a booking you MUST ensure it is permitted by the terms.
 
-							If there is a charge for the change, you MUST ask the user to consent before proceeding.
+						If there is a charge for the change, you MUST ask the user to consent before proceeding.
 						""")
 				.defaultAdvisors(
-						new MessageChatMemoryAdvisor(new InMemoryChatMemory())
+						new MessageChatMemoryAdvisor(new InMemoryChatMemory()),
+						new SimpleLoggerAdvisor()
 				)
 				.defaultTools(bookingTool)
 				.build();
 	}
 
 	@Bean
-	Map<String, PromptChatMemoryAdvisor> chatMemoryStorage() {
-		return new ConcurrentHashMap<>();
+	ChatClient calenderChatClient(ChatClient.Builder builder) {
+		return builder.build();
 	}
 
+	@Bean
+	ChatClient hrChatClient(ChatClient.Builder builder) {
+		return builder
+				.defaultSystem("""
+						You are a HR chat support agent of an IT company called "JSONPLACEHOLDER LLC". Respond in a friendly,
+						helpful, and joyful manner.
+
+						Before providing information about a employee get the following information from the user: employee id.
+						""")
+				.build();
+	}
 }
 
 @RequiredArgsConstructor
@@ -64,56 +81,117 @@ public class SpringAiAllInOneApplication {
 @Slf4j
 class PromptController {
 	private final ChatClient chatClient;
-	private final Map<String, PromptChatMemoryAdvisor> chatMemoryStorage;
+	private final ChatClient calenderChatClient;
+	private final ChatClient hrChatClient;
+	private final Map<String, PromptChatMemoryAdvisor> promptStore;
 
-	@GetMapping("{userId}/ai")
+	@GetMapping("/{id}/booking-agent")
 	@ResponseBody
-	public String getPromptResponse(@PathVariable("userId")String userId, @RequestParam String prompt) {
-		log.info("### prompt: userId={} prompt={}", userId, prompt);
-
-		//var advisor = chatMemoryStorage.computeIfAbsent(userId, k -> new PromptChatMemoryAdvisor(new InMemoryChatMemory()));
-
+	public String getPromptResponse(@PathVariable String id, @RequestParam String prompt) {
+		log.info("##prompt: {}", prompt);
 		return chatClient
 				.prompt()
 				.user(prompt)
-				/*.advisors(a -> a
-						.advisors(advisor)
-						.param(AbstractChatMemoryAdvisor.CHAT_MEMORY_CONVERSATION_ID_KEY, userId)
-						.param(AbstractChatMemoryAdvisor.CHAT_MEMORY_RETRIEVE_SIZE_KEY, 100)
-				)*/
+				.advisors(advisorSpec -> advisorSpec
+						.param(AbstractChatMemoryAdvisor.CHAT_MEMORY_CONVERSATION_ID_KEY, id)
+						.param(AbstractChatMemoryAdvisor.CHAT_MEMORY_RETRIEVE_SIZE_KEY, 100))
 				.call()
 				.content();
 	}
+
+	@GetMapping("/{id}/calender-agent")
+	@ResponseBody
+	public String getDateTimeResponse(@PathVariable String id, @RequestParam("prompt") String prompt) {
+		log.info("##datetime-agent");
+		return calenderChatClient
+				.prompt()
+				.user(prompt)
+				.advisors(advisorSpec -> advisorSpec
+						.param(AbstractChatMemoryAdvisor.CHAT_MEMORY_CONVERSATION_ID_KEY, id)
+						.param(AbstractChatMemoryAdvisor.CHAT_MEMORY_RETRIEVE_SIZE_KEY, 100))
+				.tools(new DateTimeTools())
+				.call()
+				.content();
+	}
+
+	@GetMapping("/{id}/hr-agent")
+	@ResponseBody
+	public String getHrResponse(@PathVariable String id, @RequestParam("prompt") String prompt) {
+		log.info("##hr-agent");
+		return hrChatClient
+				.prompt()
+				.user(prompt)
+				.advisors(advisorSpec -> advisorSpec
+						.param(AbstractChatMemoryAdvisor.CHAT_MEMORY_CONVERSATION_ID_KEY, id)
+						.param(AbstractChatMemoryAdvisor.CHAT_MEMORY_RETRIEVE_SIZE_KEY, 100))
+				.tools(new HrRecordTool())
+				.call()
+				.content();
+	}
+}
+
+
+class DateTimeTools {
+
+	@Tool(description = "Get the current date and time in the user's timezone")
+	String getCurrentDateTime() {
+		return LocalDateTime.now().atZone(LocaleContextHolder.getTimeZone().toZoneId()).toString();
+	}
+
+	@Tool(description = "Get the tomorrow date and time in the user's timezone")
+	String getTomorrowDate() {
+		return LocalDateTime.now().plusDays(1).atZone(LocaleContextHolder.getTimeZone().toZoneId()).toString();
+	}
+
 }
 
 @Component
 @Slf4j
 class BookingTool {
 
-	@Tool(description = "Get booking details")
-	BookingDetails getBookingDetails(@ToolParam String bookingNumber) {
-		if (bookingNumber.isEmpty()) {
-			throw new InvalidBookingNumberException(bookingNumber);
-		} else if (bookingNumber.equalsIgnoreCase("abc123")) {
-			return new BookingDetails("CONFIRMED", "Copenhagen", "New York", "2024-05-10");
-		} else {
-			throw new BookingNotFoundException(bookingNumber);
-		}
+	@Tool(description = "Get booking details using booking number")
+	BookingDetails getBookingDetails(
+			@ToolParam(description = "booking number") String bookingNumber,
+			@ToolParam(description = "customer first name") String firstName,
+			@ToolParam(description = "customer last name") String lastName) {
+		log.info("##Get booking details: {}, {}, {}", bookingNumber, firstName, lastName);
+		return new BookingDetails("CONFIRMED", "Copenhagen", "New York", "2024-05-10");
+
 	}
+
+	@Tool(description = "Change a flight booking if allowed")
+	BookingChangeResponse changeBooking(@ToolParam(description = "booking number") String bookingNumber,
+										@ToolParam(description = "customer first name") String firstName,
+										@ToolParam(description = "customer last name") String lastName) {
+		log.info("##Change booking: {}, {}, {}", bookingNumber, firstName, lastName);
+		return new BookingChangeResponse("SUCCESS", "Flight changed to 2024-05-12");
+	}
+
+	@Tool(description = "Cancel an existing flight booking")
+	CancelResponse cancelBooking(@ToolParam(description = "booking number") String bookingNumber,
+								 @ToolParam(description = "customer first name") String firstName,
+								 @ToolParam(description = "customer last name") String lastName) {
+		return new CancelResponse("CANCELLED", "Your flight has been cancelled.");
+	}
+
 }
 
 record BookingDetails(String status, String from, String to, String date) {}
+record BookingChangeResponse(String status, String message) {}
+record CancelResponse(String status, String message) {}
 
-@ResponseStatus(HttpStatus.NOT_FOUND)
-class BookingNotFoundException extends RuntimeException {
-	BookingNotFoundException(String bookingNumber) {
-		super("Booking not found: " + bookingNumber);
+class HrRecordTool {
+	@Tool(description = "Get employee details using employee id")
+	EmployeeDetails getEmployeeDetails(@ToolParam(description = "employee id") String employeeId){
+		return new EmployeeDetails(
+				"Samit",
+				"Software Engineer",
+				"IT",
+				LocalDate.of(2022, 1, 1),
+				"New York",
+				"skp@jsonplaceholder.net",
+				"+1 222 12122"
+		);
 	}
 }
-
-@ResponseStatus(HttpStatus.BAD_REQUEST)
-class InvalidBookingNumberException extends RuntimeException {
-	InvalidBookingNumberException(String bookingNumber) {
-		super("Invalid booking number: " + bookingNumber);
-	}
-}
+record EmployeeDetails(String name, String position, String department, LocalDate joiningDate, String location, String email, String phoneNumber) {}
